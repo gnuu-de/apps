67a98c98cea62367c7d3c180e9328a17f4c7b421
Frank Kloeker <eumel@admin.gnuu.de>
2020-06-29 18:43:57 +0000

    add user app

---
+++ user/user.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, redirect, url_for, request, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+import crypt
+from hmac import compare_digest as compare_hash
+from os import environ
+
+
+mysql_host = environ.get('mysql_host', 'gnuu.mysql')
+mysql_port = environ.get('mysql_port', '3306')
+mysql_user = environ.get('mysql_user')
+mysql_passwoed = environ.get('mysql_password')
+mysql_db = environ.get('mysql_db')
+
+app = Flask(__name__)
+
+app.secret_key = 'your-secret-key'
+
+app.config['MYSQL_HOST'] = mysql_host
+app.config['MYSQL_PORT'] = mysql_port
+app.config['MYSQL_USER'] = mysql_user
+app.config['MYSQL_PASSWORD'] = mysql_password
+app.config['MYSQL_DB'] = mysql_db
+
+# Intialize MySQL
+mysql = MySQL(app)
+
+@app.route('/cgi-bin/user.cgi', methods=['GET', 'POST'])
+def login():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE site = %s ', (username,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['site']
+            cryptedpasswd = account['password']
+
+            ## compare_hash(crypt.crypt(cleartext, cryptedpasswd), cryptedpasswd)
+            if compare_hash(crypt.crypt(password, cryptedpasswd), cryptedpasswd):
+            # Redirect to home page
+                return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('index.html', msg=msg)
+
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('site', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
+if __name__ == '__main__':
+
+  app.run(
+    host = "0.0.0.0",
+    port = 5000
+  )
+
